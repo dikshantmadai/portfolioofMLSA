06826a265c14befd73c021f8326b36104e051dee
dikshantmadai <122090500+dikshantmadai@users.noreply.github.com>
2023-12-27 17:10:50 +0000

    changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/ai.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Hospital Managment System",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I have created a hospital management system where users can perform various tasks related the patient information, Doctor information, Assign Doctor to the patient,Number of Patient and Doctor and  many more. I used Python to develop it.",
+    url: "https://github.com/dikshantmadai/Hospital-managemt-system",
   },
   {
-    title: "Web Development for Beginners",
+    title: "The-Assemble-Restaurant",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+"i have made a sample restaurant website using HTML/CSS",
+    url: "https://dikshantmadai.github.io/The-Assemble-Restaurant/index.html",
   },
   {
-    title: "My Resume Site",
+    title: "Configure Small LAN",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      " I have simulate small local area network.",
+    url: "https://github.com/dikshantmadai/Configure_Small_LAN",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Portfolio using React",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I have made a portfolio using react",
+    url: "https://github.com/dikshantmadai/myportfolio",
   },
 ];
 
